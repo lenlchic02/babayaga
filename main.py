73f15b94a176d15d107c354e2d6913e3a11d8285
IT-Academy-Gomel <silovielizabet0409@gmail.com>
2025-10-26 10:41:56 +0300

    начало

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-import PyQt6
+import random
+import string
+
+def generate_password(length=12, use_uppercase=True, use_numbers=True, use_special=True):
+
+    characters = string.ascii_lowercase
+
+    if use_uppercase:
+        characters += string.ascii_uppercase
+    if use_numbers:
+        characters += string.digits
+    if use_special:
+        characters += string.punctuation
+
+    if not characters:
+        raise ValueError("Должен быть выбран хотя бы один тип символов")
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+print("Случайный пароль:", generate_password())
+print("Пароль из 8 символов:", generate_password(8))
+print("Простой пароль (только буквы):", generate_password(use_numbers=False, use_special=False))
